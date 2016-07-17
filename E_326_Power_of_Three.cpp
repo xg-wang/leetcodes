7724d5952250f690000121584d7a4bb1f2828f79
Xingan Wang <wangxgwxg@gmail.com>
2016-07-17 23:43:48 +0800

    update 326. Power of Three

--- E_326_Power_of_Three.cpp
+++ E_326_Power_of_Three.cpp
@@ -6,6 +6,12 @@ using namespace std;
 class Solution {
 public:
     bool isPowerOfThree(int n) {
+        if(n == 0) return false;
+        double x = log10(n) / log10(3);
+        return floor(x) == x;
+    }
+
+    bool isPowerOfThree2(int n) {
         if (n != 1 && n % 3 != 0) return false;
         switch (n % 10) {
             case 1:
@@ -29,5 +35,6 @@ int main(int argc, char const *argv[])
 {
     Solution solver;
     cout << solver.isPowerOfThree(729) << endl;
+    cout << solver.isPowerOfThree(243) << endl;
     return 0;
 }
